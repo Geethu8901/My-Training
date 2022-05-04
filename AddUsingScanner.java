d0b6059132c2cc637ff31d7df02d805d905e10b9
Geethu8901 <geethu.annmathew@gmail.com>
2022-05-04 16:32:25 +0530

    File added

---
+++ AddUsingScanner.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class AddUsingScanner{
+	
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your  first double number");
+		double a=sc.nextDouble();
+		
+		System.out.println("Enter your second double number");
+		double b=sc.nextDouble();
+		
+			System.out.println("Sum :"+ (a+b));
+			
+		System.out.println("Enter your name");	
+		String name=sc.next();
+
+
+System.out.println("Enter Dept");	
+		String dept=sc.next();
+                char f=dept.charAt(1);
+                System.out.println("Character at first index of the string :"+ f);
+
+
+		
+		
+	}
+}
